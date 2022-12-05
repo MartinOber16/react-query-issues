75a9ed09e291793a92256b9faea1470ae50f6f88
Martin Obermeier <martin.obermeier@gmail.com>
2022-12-05 16:30:01 -0300

    Seccion 7: paginacion

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -8,7 +8,10 @@ import { State } from '../interfaces';
 export const ListView = () => {
 	const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
 	const [state, setState] = useState<State>();
-	const { issuesQuery } = useIssues({ state, labels: selectedLabels });
+	const { issuesQuery, page, nextPage, prevPage } = useIssues({
+		state,
+		labels: selectedLabels,
+	});
 
 	const onLabelChange = (labelName: string) => {
 		selectedLabels.includes(labelName)
@@ -28,6 +31,22 @@ export const ListView = () => {
 						onStateChanged={newState => setState(newState)}
 					/>
 				)}
+
+				<div className='d-flex mt-2 justify-content-between align-item-center '>
+					<button
+						className='btn btn-outline-primary'
+						onClick={prevPage}
+						disabled={issuesQuery.isFetching}>
+						Prev
+					</button>
+					<span>{page}</span>
+					<button
+						className='btn btn-outline-primary'
+						onClick={nextPage}
+						disabled={issuesQuery.isFetching}>
+						Next
+					</button>
+				</div>
 			</div>
 
 			<div className='col-4'>
